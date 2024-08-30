7687225be5e4c5210596bdad6fc08c44cb6d2dcf
Poorna Malinhara Kuruppu Arachchi <131478375+Malinhara@users.noreply.github.com>
2024-08-30 22:27:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const http = require('http');
 const PORT = 3000;
+const host = 0.0.0.0;
 
+// Create the server
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello sri Lanka!');
+  res.end('Hello Sri Lanka!');
 });
 
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+// Listen on all network interfaces (0.0.0.0) and specified port
+server.listen(PORT,host, () => {
+  console.log(`Server running at http://0.0.0.0:${PORT}/`);
 });
